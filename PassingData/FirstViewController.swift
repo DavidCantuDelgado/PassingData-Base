f11dee282792127c67a7265cf8c16f4f255ecdfe
David Cantu <david.cantu.delgado@tec.mx>
2019-02-06 17:25:27 -0600

    Initial Commit

---
+++ PassingData/FirstViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PassingData
+//
+//  Created by David A Cantú Delgado on 2/6/19.
+//  Copyright © 2019 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var tfTexto1: UITextField!
+    
+    @IBOutlet weak var lbTexto1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    @IBAction func EnviaTexto(_ sender: UIButton) {
+    }
+    
+
+}
+
---
+++ PassingData/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  PassingData
+//
+//  Created by David A Cantú Delgado on 2/6/19.
+//  Copyright © 2019 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    
+    @IBOutlet weak var tfTexto2: UITextField!
+    
+    @IBOutlet weak var lbTexto2: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func RegresaTexto(_ sender: UIButton) {
+    }
+    
+}
